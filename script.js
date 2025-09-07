bee886603e6ca389850b02e1a3274a22157e7a0c
ATP-co <AdvancedTechProgramming@outlook.com>
2025-09-07 00:00:02 -0600

    Primera Version

---
+++ script.js
@@ -0,0 +1,13 @@
+function mostrarFormulario() {
+    const formulario = document.getElementById('formulario-contacto');
+    formulario.style.display = 'grid';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('formulario-contacto');
+    form.addEventListener('submit', function (e) {
+      e.preventDefault(); // evita que recargue la página
+      form.style.display = 'none'; // oculta el formulario
+      alert('¡Gracias por tu mensaje!'); // puedes personalizar este mensaje
+    });
+});
